e98921075fdf882612e7a3c1e203f8ec1da45a4c
root <omarali43620@yahoo.com>
2020-02-03 12:40:43 +0100

    Updates

---
+++ precache-manifest.2a950e89212ecf04135cc60a7c020c05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e63bffd7679a1ef15a45a131b86bebfc",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "8f5ddf135bea6e505070",
+    "url": "/Calculator/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a36eccecb7342514f0a9",
+    "url": "/Calculator/static/js/2.63521b76.chunk.js"
+  },
+  {
+    "revision": "b5a01430b9928300ee798657f45fe7d4",
+    "url": "/Calculator/static/js/2.63521b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f5ddf135bea6e505070",
+    "url": "/Calculator/static/js/main.516abfa9.chunk.js"
+  },
+  {
+    "revision": "196d2c63677881e0d8e0",
+    "url": "/Calculator/static/js/runtime-main.e08c1cc7.js"
+  }
+]);
